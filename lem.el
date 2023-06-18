ef50d8984d9f09ec9428227be53694657b53a334
marty hiatt <martianhiatus [a t] riseup [d o t] net>
2023-06-18 22:38:28 +0200

    re-order, and re-write w macro

--- lem.el
+++ lem.el
@@ -29,77 +29,51 @@
 ;;; Code:
 
 (require 'fedi-http)
+(require 'persist)
 
 (setq fedi-http--api-version "v3")
 
 (defvar fedi-instance-url "https://lemmy.ml")
 
-(defvar lem-auth-token nil)
+(persist-defvar lem-auth-token nil
+                "A user auth token for a lemmy instance.")
 
-(defun lem-site ()
-  "Return detauls about instance at `lem-instance-url'."
-  (let ((url (fedi-http--api "site")))
-    (fedi-http--get-json url)))
+;;; MACRO
+(defmacro lem-def-request (method name endpoint &optional args params json)
+  "Create http request function NAME, using http METHOD, for ENDPOINT.
+ARGS are for the function, PARAMS is an alist of form parameters.
+JSON means to send params as a JSON payload."
+  (declare (debug t)
+           (indent 1))
+  (let ((req-fun (intern (concat "fedi-http--" method))))
+    `(defun ,(intern (concat "lem-" name)) ,args
+       (let* ((url (fedi-http--api ,endpoint))
+              (response
+               (cond ((equal ,method "post")
+                      (funcall #',req-fun url ,params nil :unauthed ,json))
+                     ((equal ,method "get")
+                      (funcall #',req-fun url ,params)))))
+         (fedi-http--triage response
+                            (lambda ()
+                              (with-current-buffer response
+                                (fedi-http--process-json))))))))
 
-(defun lem-list-communities ()
-  "List communities on the instance."
-  (let ((url (fedi-http--api "community/list")))
-    (fedi-http--get-json url)))
+;;; INSTANCE
+(lem-def-request "get" "instance" "site")
 
-(defun lem-list-instance-posts ()
-  "Get the instance's list of posts, like its home page."
-  (let ((url (fedi-http--api "post/list")))
-    (fedi-http--get-json url)))
+;; (lem-instance)
 
-(defun lem-community (id)
-  "Get community with ID. Returns community_view object."
-  (let* ((params `(("id" . ,id)))
-         (url (fedi-http--api "community")))
-    (fedi-http--get-json url params)))
+(lem-def-request "get" "get-instance-posts" "post/list")
 
-(defun lem-get-post (id) ; &optional auth
-  "Get post with ID, for display.
-Returns post_view, community_view and moderators objects."
-  ;; post_view contains: post, creator, community,
-  ;; creator_banned_from_community, counts, etc.
-  (let* ((params `(("id" . ,id)))
-         (url (fedi-http--api "post")))
-    (fedi-http--get-json url params)))
+;; (lem-get-instance-posts)
 
-(defun lem-like-post (id)
-  "Like post with ID. Requires auth."
-  (let* ((params `(("id" . ,id)))
-         (url (fedi-http--api "post/like")))
-    (fedi-http--get-json url params)))
+(lem-def-request "get" "get-communities" "community/list")
 
-(defun lem-report-post (id reason)
-  "Report post with ID, providing REASON."
-  (let* ((params `(("post_id" . ,id)
-                   ("reason" . ,reason)
-                   ("auth" . ,lem-auth-token)))
-         (url (fedi-http--api "post/report")))
-    (fedi-http--get-json url params)))
-
-(defun lem-get-post-comments (post-id)
-  "Get comments for POST-ID.
-Returns comments,a list of comment objects, for display."
-  (let ((params `(("post_id" . ,post-id)))
-        (url (fedi-http--api "comment/list")))
-    (fedi-http--get-json url params)))
-
-(defun lem-community-posts (id) ; limit page &optional auth
-  "Get posts for community with ID.
-Returns posts, for listing not viewing."
-  (let* ((params `(("community_id" . ,id)))
-         (url (fedi-http--api "post/list")))
-    (fedi-http--get-json url params)))
-
-(defun lem-search (query)
-  "Search for QUERY.
-Returns comments, posts, communities objects."
-  (let ((params `(("q" . ,query)))
-        (url (fedi-http--api "search")))
-    (fedi-http--get-json url params)))
+;;; SEARCH
+(lem-def-request "get"
+  "search" "search"
+  (query)
+  `(("q" . ,query)))
 
 (defun lem-map-community-ids-names (communities)
   "Return an alist of id and name for each item in COMMUNITIES."
@@ -117,38 +91,10 @@ Returns comments, posts, communities objects."
          (communities (alist-get 'communities (lem-search query)))
          (choice (completing-read "Community: "
                                   (lem-map-community-ids-names communities)))) ;
-    ;; (lem-get-community choice))) ; returns community_view, its own info
-    (lem-community-posts choice))) ; returns community's posts
+    (lem-get-community choice))) ; returns community_view, its own info
+;; (lem-community-posts choice))) ; returns community's posts
 
-(defun lem-get-person-by-id (id) ; &optional auth
-  "Return details for user with ID.
-Returns person_view, which has person, comments, posts, and moderates."
-  (let ((params `(("person_id" . ,id)))
-        (url (fedi-http--api "user")))
-    (fedi-http--get-json url params)))
-
-(defun lem-get-person-by-name (name) ; &optional auth
-  "Return details for user with NAME.
-Returns person_view, which has person, comments, posts, and moderates."
-  (let ((params `(("username" . ,name)))
-        (url (fedi-http--api "user")))
-    (fedi-http--get-json url params)))
-
-(defmacro lem-def-request (method name endpoint args params &optional json)
-  "Create http request function NAME, using http METHOD, for ENDPOINT.
-ARGS are for the function, PARAMS is an alist of form parameters.
-JSON means to send params as a JSON payload."
-  (declare (debug t)
-           (indent 1))
-  (let ((req-fun (intern (concat "fedi-http--" method))))
-    `(defun ,(intern (concat "lem-" name)) ,args
-       (let* ((url (fedi-http--api ,endpoint))
-              (response (funcall #',req-fun url ,params nil :unauthed ,json)))
-         (fedi-http--triage response
-                            (lambda ()
-                              (with-current-buffer response
-                                (fedi-http--process-json))))))))
-
+;;; AUTH
 (lem-def-request "post"
   "login" "user/login"
   (name password)
@@ -161,6 +107,31 @@ JSON means to send params as a JSON payload."
   (interactive)
   (let ((json (lem-login name password)))
     (setq lem-auth-token (alist-get 'jwt json))))
+
+
+;;; USERS
+(lem-def-request "get"
+  "get-person-by-id" "user"
+  (id)
+  `(("person_id" . ,id)))
+
+;; (lem-get-person-by-id "8511")
+
+(lem-def-request "get"
+  "get-person-by-name" "user"
+  (name)
+  `(("username" . ,name)))
+
+;; (lem-get-person-by-name "blawsybogsy")
+
+;;; COMMUNITIES
+(lem-def-request "get"
+  "get-community" "community"
+  (id)
+  `(("id" . ,id)
+    ("auth" . ,lem-auth-token)))
+
+;; (lem-get-community "96200")
 
 (lem-def-request "post"
   "follow-community" "community/follow"
@@ -179,17 +150,40 @@ JSON means to send params as a JSON payload."
 ;;   (when (equal subed "Subscribed")
 ;;     (format "Subscribed to %s [%s]" name desc)))
 
+
+;; TODO: CreateCommunity
+;; TODO: DeleteCommunity
+
+;;; POSTS
+(lem-def-request "get"
+  "get-post" "post"
+  (id)
+  `(("id" . ,id)
+    ("auth" . ,lem-auth-token)))
+
+;; (lem-get-post "1341246")
+
+(lem-def-request "get"
+  "list-posts" "post/list"
+  (community-id) ; &optional limit page sort type
+  `(("community_id" . ,community-id)
+    ("auth" . ,lem-auth-token)))
+;; ("limit" . ,limit)
+;; ("page" . ,page)))
+
+;; (lem-list-posts "96200")
+
 (lem-def-request "post"
-"create-post" "post"
-(name community-id &optional body url nsfw honeypot) ; lang-id
-`(("community_id" . ,community-id)
-  ("auth" . ,lem-auth-token)
-  ("name" . ,name)
-  ("body" . ,body)
-  ("url" . ,url)
-  ("nsfw" . ,nsfw)
-  ("honeypot" . ,honeypot))
-:json)
+  "create-post" "post"
+  (name community-id &optional body url nsfw honeypot) ; lang-id
+  `(("community_id" . ,community-id)
+    ("auth" . ,lem-auth-token)
+    ("name" . ,name)
+    ("body" . ,body)
+    ("url" . ,url)
+    ("nsfw" . ,nsfw)
+    ("honeypot" . ,honeypot))
+  :json)
 
 ;; cb:
 ;; (let* ((json (fedi-http--process-json))
@@ -197,6 +191,42 @@ JSON means to send params as a JSON payload."
 ;;        (name (alist-get 'name post)))
 ;;   (when name
 ;;     (format "Post created: %s" name)))
+
+(lem-def-request "get"
+  "like-post" "post/like"
+  (id)
+  `(("id" . ,id)
+    ("auth" . ,lem-auth-token)))
+
+;; (lem-like-post "1341264")
+;; (lem-like-post "1341246")
+;; (lem-like-post "765662")
+
+;; (defun lem-like-post (id)
+;;   "Like post with ID. Requires auth."
+;;   (let ((params `(("id" . ,id)
+;;                   ("auth" . ,lem-auth-token)))
+;;         (url (fedi-http--api "post/like")))
+;;     (fedi-http--get-json url params)))
+
+;; TODO: edit post
+
+(lem-def-request "post"
+  "report-post" "post/report"
+  (id reason)
+  `(("post_id" . ,id)
+    ("reason" . ,reason)
+    ("auth" . ,lem-auth-token))
+  :json)
+
+;;; COMMENTS
+(lem-def-request "get"
+  "get-comment" "comment"
+  (id)
+  `(("id" . ,id)
+    ("auth" . ,lem-auth-token)))
+
+;; (lem-get-comment "765662")
 
 (lem-def-request "post"
   "create-comment" "comment"
@@ -206,6 +236,18 @@ JSON means to send params as a JSON payload."
     ("content" . ,content)
     ("parent_id" . ,parent-id))
   :json)
+
+(lem-def-request "get"
+  "get-post-comments" "comment/list"
+  (post-id)
+  `(("post_id" . ,post-id)
+    ("auth" . ,lem-auth-token)))
+
+;; (lem-get-post-comments "1341246")
+
+;; TODO: edit comment
+;; TODO: report comment
+;; TODO: list community comments
 
 ;; cb:
 ;; (let* ((json (fedi-http--process-json))
