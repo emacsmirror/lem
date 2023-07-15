53cd1aed6c3dc9987ab6eb74cf3b40a20172a6c8
marty hiatt <martianhiatus [a t] riseup [d o t] net>
2023-07-15 12:55:43 +0200

    url regex: community may have _ char

--- lisp/lem-ui.el
+++ lisp/lem-ui.el
@@ -524,7 +524,7 @@ STRING means ID should be a string."
           (string-match "^/p/[[:alpha:]]+/[[:digit:]]+$" query)
           (string-match "^/[[:alpha:]]+$" query)
           (string-match "^/u/[_[:alpha:]]+$" query)
-          (string-match "^/c/[[:alnum:]]+$" query)
+          (string-match "^/c/[_[:alnum:]]+$" query)
           (string-match "^/post/[[:digit:]]+$" query)
           (string-match "^/comment/[[:digit:]]+$" query)))))
 
