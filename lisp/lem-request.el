64cd28f8ca7f5ac110877ae7f942120b510679ce
marty hiatt <martianhiatus [a t] riseup [d o t] net>
2023-10-16 16:13:58 +0200

    def-request: uncomma lem-auth-token (ie compute it in the defuns)

--- lisp/lem-request.el
+++ lisp/lem-request.el
@@ -84,7 +84,7 @@ Q is the search query.\"
     `(defun ,(intern (concat "lem-" name)) ,args
        ,docstring
        (let* ((req-url (fedi-http--api ,endpoint lem-instance-url lem-api-version))
-              (auth-token ,lem-auth-token)
+              (auth-token lem-auth-token)
               (auth-header `(("Authorization" .
                               ,(concat "Bearer " auth-token))))
               (url-request-method ,(upcase method))
