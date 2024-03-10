491c106a8c580d834bde33f36b07c79d995b2b6e
marty hiatt <martianhiatus@riseup.net>
2024-03-10 16:27:39 +0100

    add @ and . to user fedilike url regex

--- lisp/lem-ui.el
+++ lisp/lem-ui.el
@@ -1072,7 +1072,7 @@ STRING means ID should be a string."
           (string-match "^/profile/[[:alpha:]]+$" query)
           (string-match "^/p/[[:alpha:]]+/[[:digit:]]+$" query)
           ;; (string-match "^/[[:alpha:]]+$" query) ; unsinn! this matches https://example.com/example
-          (string-match "^/u/[_[:alpha:]]+$" query)
+          (string-match "^/u/[@._[:alpha:]]+$" query)
           (string-match "^/c/[@._[:alnum:]]+$" query)
           (string-match "^/post/[[:digit:]]+$" query)
           (string-match "^/comment/[[:digit:]]+$" query)))))
